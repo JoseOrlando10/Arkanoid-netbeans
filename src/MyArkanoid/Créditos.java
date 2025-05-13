e6783a92d5629868ac63a16fe8e97ad6e9587a5c
Jose Martins <108726841+JoseOrlando10@users.noreply.github.com>
2025-05-13 21:06:42 +0100

    Jogo/Creditos já aparece ao centro

--- src/MyArkanoid/Créditos.java
+++ src/MyArkanoid/Créditos.java
@@ -18,6 +18,7 @@ public class Créditos extends javax.swing.JDialog {
     public Créditos(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
         initComponents();
+        setLocationRelativeTo(null);// coloca no centro do ecra
     }
 
     /**
